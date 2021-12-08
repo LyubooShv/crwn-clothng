eeede2f72312dece415645c961de797f9810d118
LyubooShv <liubomirshivachev@gmai.com>
2021-12-08 18:13:02 +0200

    firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,30 @@ const config = {
     appId: "1:158947489676:web:22eeca45310ab4246b88a9",
     measurementId: "${config.measurementId}"
   };
+
+  firebase.initializeApp(config);
+  
+  export const createUserProfileDocument = async (userAuth, additionalData)=> {
+    if (!userAuth) return;
+
+    const userRef=firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot=await userRef.get();
+
+    if(!snapShot.exists){
+      const {displayName,email}=userAuth;
+      const createdAt=new Date();
+
+      try{
+await userRef.set({
+  displayName,email,createdAt,...additionalData
+})
+      } catch (error) {
+console.log('error creating user',error.message);
+      }
+    }
+    return userRef;
+  }
   
   firebase.initializeApp(config);
 
